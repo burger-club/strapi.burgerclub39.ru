3f9d6726468560c1b444148be4aaa0335b3ad97b
Alexey Filippov <socket.someone@gmail.com>
2022-07-08 20:02:45 +0300

    Update server.js

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: "http://strapi.yoursburgers.ru",
+  url: "https://strapi.yoursburgers.ru",
   app: {
     keys: env.array('APP_KEYS'),
   },
